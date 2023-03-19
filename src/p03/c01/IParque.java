94d553ee19e98e48ecfe67409495b1f5cace3ffc
pac1006 <117029295+pac1006@users.noreply.github.com>
2023-03-19 09:36:25 +0000

    Update IParque.java
    Implementación completa de la interfaz IParque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,15 @@ package src.p03.c01;
 
 public interface IParque {
 	
+	/**
+	 * Gestiona la entrada al parque
+	 */
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
+	/**
+	 * Gestiona la salida del parque
+	 */
+	public abstract void salirDelParque(String puerta);
+
 
 }
